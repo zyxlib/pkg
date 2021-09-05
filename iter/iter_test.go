b8aa9920606ee6d0cc15ca1630ca25810f426b4e
Injun Song <ijsong@zyx-kakao.local>
2021-09-05 12:58:46 +0900

    iter: add Range
    Range returns a slice from zero to n-1.

--- iter/iter_test.go
+++ iter/iter_test.go
@@ -6,6 +6,37 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestRange(t *testing.T) {
+	testCases := []struct {
+		name  string
+		testF func(*testing.T)
+	}{
+		{
+			name: "Zero",
+			testF: func(t *testing.T) {
+				require.Len(t, Range(0), 0)
+			},
+		},
+		{
+			name: "One",
+			testF: func(t *testing.T) {
+				require.Equal(t, []int{0}, Range(1))
+			},
+		},
+		{
+			name: "Ten",
+			testF: func(t *testing.T) {
+				require.Equal(t, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, Range(10))
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, tc.testF)
+	}
+}
+
 func TestPermutation(t *testing.T) {
 	testCases := []struct {
 		name  string
